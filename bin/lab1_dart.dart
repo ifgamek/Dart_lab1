2fa550df96b3bf0abee7315aab856869b78b0996
ifgamek <andkategik@gmail.com>
2026-09-15 18:09:02 +0300

    learn dart syntax and types

--- bin/lab1_dart.dart
+++ bin/lab1_dart.dart
@@ -1,33 +1,54 @@
 import 'package:lab1_dart/lab1_dart.dart' as lab1_dart;
-string greet(string name)
-  return 'Привет, $name!';
-void main (){
-  print(greet('артем'))
-  print(greet('мария'))
+
+void main() 
+{
+  String name = 'артем';
+  int age = 20;
+  double height = 1.75;
+  bool isstudents = true;
+
+  print(name);
+  print(age);
+  print(height);
+  print(isstudents);
+
+print('привет, {name}! тебе {age} лет.');
+print('через 5 лет тебе будет ${age +5} лет.');
+print('рост ${height} м , студент :$isstudents');
+
+var score = 95;
+var language = 'dart';
+print('$language : $score');
+
+const String appname = 'lab1';
+final int startyear = 2026;
+print('$appname started in $startyear');
+
+String? city = null;
+if (city != null) {print(city.toUpperCase(),);}
+print(city?. toUpperCase());
+
+String? nickname = null;
+String display = nickname ?? 'аноним';
+print(display);
+
+List<String> fruits = ['яблоко','банан','груша'];
+fruits.add('апельсин');
+print(fruits[0]);
+print(fruits.length);
+
+Map<String, dynamic> person = {'name': 'артем', 'age': 20};
+print(person['name']);
+person['city'] = 'волжский';
+
+
+
+Set<int> ids = {1,2,3,2,1};
+print(ids);
+print(ids.length);
+
+List<String> fruits2 = ['яблоко' , 'банан' , 'груша'];
+for (var fruits in fruits2){
+  print(fruits);}
 }
 
-
-
-void descridepet({required string name, string species = 'кот' , int age = 0})
-{print('$name - $species , возраст = $age');
-}
-descridepet(name:'барсик', age : 3)
-descridepet(name: 'шарик', species : 'пес')
-
-string repeat(string text, [int times = 2]){
-  string result = '';
-  for (int i = 0; i < times; i ++ ){result += text;}
-  return result;
-}
-print(repeat('ха'));
-print(repeat('ха' , 3));
-
-List<int> numbers = [3,1,4,1,5,9];
-numbers.sort((a,b) => b - a)
-print(numbers);
-
-List<string> upper = names.map((name) => name.touppercase()).tolist();
-print(upper);
-
-List<string> longnames = names.where((name) => name.length > 4).tolist();
-print(longnames)
